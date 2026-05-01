4967a002f9c700701d50bd85e65fe46e148135ef
Pushkar2103 <2k22.csai.2213231@gmail.com>
2026-05-01 09:17:45 +0530

    server.js update

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ const startServer = async () => {
       });
     });
 
-    const PORT = process.env.PORT;
+    const PORT = process.env.PORT || 3000;
 
     app.listen(PORT, "0.0.0.0", () => {
         console.log(`Server running on port ${PORT}`);
